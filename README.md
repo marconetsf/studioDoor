65cf85db3596634863bff6cdd7b9b3d07ceeccf3
marconetsf <marconetsf@gmail.com>
2020-03-17 15:26:14 -0300

    adding the readme file

---
+++ README.md
@@ -0,0 +1,32 @@
+# About the project
+This program will conversating via HTTP with a local servidor that will care about recongnize people via OpenCV + python script. When the system recognizes a person it will send a request to an ESP8266 that will control the oppening of the entrance door. 
+
+# About the ESP code
+
+Algorithm - When the ESP8266 working as a WebServer receive a http request it will send de right pulse to control a servo that will moviment the door to open. When the door open, a sensor (that will be installed ahead of the door) will start to captate the presence in a small perimete to recognize when the person left the door range and close it. 
+
+# About the facial recofnition
+
+xxx
+
+# PIR Sensor
+
+The tecnical name of it is HC-SR501, this module can monitoring the presence of moviment in an adjustable range. 
+"Once movement is detected the output goes high and stays high only as long as the timer setting dictates, even if there is continuous movement. It then goes low and stays low for a preset period when the sensor is disabled. Then it looks for movement again and goes high when it sees it."
+
+| Specifications | Values |
+| -------------  | ------ |
+| Voltage | 5 ~ 20 V|
+| Output Voltage | 3.3 ~ 0V|
+| Sensing Delay | 0.3 ~ 5min|
+| Sensing range | less than 120° |
+
+# Electrical Connection Diagram
+
+
+![Untitled Sketch 2_bb](https://user-images.githubusercontent.com/37492251/76888371-91c65780-6862-11ea-9fda-7c378fadb244.png)
+
+***Observations about schematich is that this Hi-link component can be replaced by any supply with 5v in the output. 
+Theses two resistors are making a voltage divider, that turn 5v in 3.3v (which is te recommended for esp8266)***
+
+
--- studioDoor/studioDoor.ino
+++ studioDoor/studioDoor.ino
@@ -1,19 +1,18 @@
 /*********
   STOP and PLAY
 
-  Program Algorithm 
+  Program Algorithm
 
     When the camera recongnizes the face it will send a request for esp and then the door will open
-    When the door open, an pir sensor will be installed ahead of the door to see when the person left of the door range. When it happen, door will close 
-  
+    When the door open, an pir sensor will be installed ahead of the door to see when the person left of the door range. When it happen, door will close
+
 *********/
 
 #include <ESP8266WiFi.h>      // Load Wi-Fi library
 #include <ESP8266WebServer.h> // Web server library
 #include <ArduinoOTA.h>       // Over the air
-#include <Servo.h>
+#include <Servo.h>            // Motor library 
 
-// Replace with your network credentials
 const char* ssid = "MAPLE ADM";
 const char* password = "mapleadm";
 const int pirSensor = D6;
@@ -23,7 +22,7 @@ ESP8266WebServer server(80);  // Iniciate an objetc of ESP8266WebServer class
 
 IPAddress staticIP(192, 168, 10, 132);
 IPAddress gateway(192, 168, 10, 1);
-IPAddress subnet(255, 255, 255, 0); 
+IPAddress subnet(255, 255, 255, 0);
 
 const int greenLed = D3;
 const int redLed = D4;
@@ -31,18 +30,16 @@ const int redLed = D4;
 bool door1 = false;
 bool sensorStatus = false;
 
-////////////////////////////////////////////////////////////////////////
 void setup() {
-  Serial.begin(115200); // Inicialize a Serial communication with the computer with 9600 of baud rate
-  door.attach(D5);
-  // Initialize the output variables as outputs
+  Serial.begin(115200);
 
+  door.attach(D5);
   pinMode(greenLed, OUTPUT);
   pinMode(redLed, OUTPUT);
   pinMode(pirSensor, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
-  // Connect to Wi-Fi network with SSID and password
+  // WiFi Connection
   Serial.print("Connecting to ");
   Serial.println(ssid);
   WiFi.config(staticIP, gateway, subnet);
@@ -51,7 +48,7 @@ void setup() {
     delay(500);
     Serial.print(".");
   }
-  
+
   // Print local IP address and start web server
   Serial.println("");
   Serial.println("WiFi connected.");
@@ -59,8 +56,8 @@ void setup() {
   Serial.println(WiFi.localIP());
 
   pisca(10, 200);
-  
-  server.on("/opendoor", ligar1);
+
+  server.on("/opendoor", abrirPorta);
   server.begin();
 
   beginOTA();
@@ -68,30 +65,29 @@ void setup() {
 
 void loop() {
   ArduinoOTA.handle();    // Function who listen if a WiFi boot is comming
-  server.handleClient();  // Function who listen the requests pre-configured on setup funciont
+  server.handleClient();  // Function who listen the requests pre-configured on setup function
 
-  // When the wifi signal is interrupted the esp's led goes on 
-  if (WiFi.status() == true){  
+  // When the wifi signal is interrupted the esp's led goes on
+  if (WiFi.status() == true) {
     digitalWrite(LED_BUILTIN, LOW);
   } else {
     digitalWrite(LED_BUILTIN, HIGH);
   }
-  
-  if (door1 == true){ // Set on the led for 3 seconds 
-    sensorStatus = digitalRead(pirSensor);
-    if (sensorStatus == true){
+
+  if (door1 == true) {
+    sensorStatus = digitalRead(pirSensor); // Checking the sensor status
+    if (sensorStatus == true) {
       door.write(0);
     }
   }
 
-  yield(); // Function that ensures that the background functions of esp will keep responding, like tcpip protocol, wifi connection etc... 
+  yield(); // Function that ensures that the background functions of esp will keep responding, like tcpip protocol, wifi connection etc...
 }
 
-//////////////////////// FUNCTIONS ////////////////////////////////////
-void ligar1() {                 // Open the First Door
-  door.write(90);  // set HIGH the D1 port 
-  delay(500);                   // Delay for 0.3 seconds
-  server.send(200, "text/html", "{\"status\":\"recebido\"}"); //send a json response to the client 
+void abrirPorta() {
+  door.write(90);
+  server.send(200, "text/html", "{\"status\":\"recebido\"}"); //send a json response to the client
+  delay(500);
   door1 = true;
 }
 
@@ -105,27 +101,27 @@ void pisca(int vezes, int tempo) {
 }
 
 // Function OTA - Over the Air
-void startOTA(){
+void startOTA() {
   String type;
-  
+
   // If the update is being engravin on flash mame it returns "flash"
   if (ArduinoOTA.getCommand() == U_FLASH) type = "flash";
   else type = "filesystem"; // U_SPIFFS // If the update is being engraving by intern memory it returns "filesystem"
-  
+
   //Show the returned information
   Serial.println("Start updating " + type);
 }
 
-void endOTA(){
+void endOTA() {
   Serial.println("\nEnd");
 }
 
-// It shows the progress of engraving 
-void progressOTA(unsigned int progress, unsigned int total){
+// It shows the progress of engraving
+void progressOTA(unsigned int progress, unsigned int total) {
   Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
 }
 
-void errorOTA(ota_error_t error){
+void errorOTA(ota_error_t error) {
   Serial.printf("Error[%u]: ", error);
 
   if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
@@ -135,7 +131,7 @@ void errorOTA(ota_error_t error){
   else if (error == OTA_END_ERROR) Serial.println("End Failed");
 }
 
-void beginOTA(){
+void beginOTA() {
   //define what function will be run when the OTA process begins
   ArduinoOTA.onStart(startOTA);
 
